b143ef908bc0cae46377eee81c58bde657d4a755
RibizliRoka <lauraszabo777@gmail.com>
2026-05-23 00:28:09 -0500

    organization

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,12 @@
 module.exports = function(eleventyCc) {
-  eleventyCc.addPassthroughCopy("*.css");
-  eleventyCc.addPassthroughCopy("*.js");
+  eleventyCc.addPassthroughCopy("src/css");
+  eleventyCc.addPassthroughCopy("src/scripts");
+  eleventyCc.addPassthroughCopy("markdown");
+  eleventyCc.addPassthroughCopy("components");
 
   return {
     dir: {
-      input: ".",
+      input: "src",
       output: "_site"
     }
   };
